90079da57450c2b7fea34a093d7f0460fc342ca2
Joshua Jones <joshua.ar.jones@gmail.com>
2024-09-08 15:03:33 +0100

    fix: resolve issues in bin packing function

--- steel_opt.py
+++ steel_opt.py
@@ -4,113 +4,130 @@ lengths to use for a steel structure cut from stock
 """
 
 
-def add_to_cut_list():
-    partSize = input("Please enter your cut length in mm: ")
-    partSize = int(partSize)
-    partQuant = input("How many do you have? ")
-    partQuant = int(partQuant)
-    print("Total Stock Length available is: " + str(totalStock))
-    i = 0
-    while i < partQuant:
-        cut_list.append(partSize)
-        i += 1
-    return cut_list
-
-
-""" Partition a list into sublists whose sums don't exceed a maximum
-    using a First Fit Decreasing algorithm. See
-    http://www.ams.org/new-in-math/cover/bins1.html
-    for a simple description of the method.
-"""
-
-
 class Bin(object):
-    """Container for items that keeps a running sum"""
+    """Container for items that keeps a running sum of how much of the stock has been used"""
 
-    def __init__(self):
+    def __init__(self, capacity):
+        if capacity <= 0:
+            raise ValueError()
+        self.capacity = capacity
         self.items = []
-        self.sum = 0
+        self.currently_used = 0
 
-    def append(self, item):
+    def update(self, item):
         self.items.append(item)
-        self.sum += item
+        self.currently_used = sum(self.items)
 
     def __str__(self):
         """Printable representation"""
-        return "Bin(sum=%d, items=%s)" % (self.sum, str(self.items))
+        return "Bin(capacity=%d, currently_used=%d, cuts=%s)" % (
+            self.capacity,
+            self.currently_used,
+            str(self.items),
+        )
 
 
-def pack(values, maxValue):
-    values = sorted(values, reverse=True)
+class StockInfo:
+    def __init__(self):
+        self.stock_length = int(
+            input(
+                "Please enter your stock length in mm:  (for example you might purchase 1m tubes in which case enter 1000): "
+            )
+        )
+        self.number_of_stock = int(input("How many do you have? "))
+        self.total_length = self.stock_length * self.number_of_stock
+        print(f"Stock Length is: {self.stock_length}")
+        print(f"Total Stock Length is: {self.total_length}")
+
+    def check(self, cut_list):
+        if sum(cut_list) > self.total_length:
+            print("Not enough stock.")
+            quit()
+
+    def __str__(self):
+        """Printable representation"""
+        return "StockInfo(stock_length=%d, number_of_stock=%d, total_length=%d)" % (
+            self.stock_length,
+            self.number_of_stock,
+            self.total_length,
+        )
+
+
+def pack(values, bin_size):
+    """
+    Pack a list of values into bins where the sum of the values in each bin
+    does not exceed bin_size.
+
+    The algorithm used is First Fit Decreasing, described at
+    https://www.ams.org/publicoutreach/feature-column/fcarc-bins2
+    """
+    if max(values) > bin_size:
+        raise ValueError("bin_size too small")
+    if len(values) == 0:
+        raise ValueError("no values")
     bins = []
+    cuts = sorted(values, reverse=True)
+    print("cuts", cuts)
+    # create first bin
+    bins.append(Bin(capacity=bin_size))
 
-    for item in values:
+    # iterate over cuts and place in bins
+    for cut in cuts:
+        current_bin = bins[-1]
         # Try to fit item into a bin
-        for bin in bins:
-            if bin.sum + item <= maxValue:
-                # print 'Adding', item, 'to', bin
-                bin.append(item)
-                break
+
+        print(f"currently used = {current_bin.currently_used} cut = {cut}")
+        if current_bin.currently_used + cut < current_bin.capacity:
+            current_bin.update(cut)
         else:
-            # item didn't fit into any bin, start a new bin
-            # print 'Making new bin for', item
-            bin = Bin()
-            bin.append(item)
-            bins.append(bin)
+            print("bin full")
+            new_bin = Bin(capacity=bin_size)
+            new_bin.update(cut)
+            print("new bin", new_bin)
+            bins.append(new_bin)
 
     return bins
 
 
-def pack_and_show(aList, maxValue):
-    """Pack a list into bins and show the result"""
+def new_section(message):
+    print("-" * 80)
+    print(message)
+
+
+if __name__ == "__main__":
+    # get input for bin sizes and total number of bins
+    new_section("Input starting materials")
+
+    stock_info = StockInfo()
+
+    new_section("Input cut list")
+
+    cut_lengths = []
+    answer = "y"
+    while answer == "y":
+        length = int(input("Please enter your cut length in mm: "))
+        quantity = int(input("How many do you have? "))
+        cut_lengths.extend([length] * quantity)
+
+        print(f"Current Length is: {sum(cut_lengths)} mm")
+
+        stock_info.check(cut_lengths)
+        answer = input("do you have more to add? y/n?: ")
+
+    cut_lengths.sort()
+    print(cut_lengths)
+    print(stock_info)
     print(
         "List with sum",
-        sum(aList),
+        sum(cut_lengths),
         "requires at least",
-        (sum(aList) + maxValue - 1) / maxValue,
+        (sum(cut_lengths) + stock_info.stock_length - 1) / stock_info.stock_length,
         "bins",
     )
-    bins = pack(aList, maxValue)
+    bins = pack(cut_lengths, stock_info.stock_length)
 
     print("Solution using", len(bins), "bins:")
     for bin in bins:
         print(bin)
-
-
-if __name__ == "__main__":
-    print("-" * 80)
-    print("Input starting materials")
-    stock_length = int(
-        input(
-            "Please enter your stock length in mm:  (for example you might purchase 1m tubes in which case enter 1000): "
-        )
-    )
-    quantity = int(input("How many do you have? "))
-
-    totalStock = stock_length * quantity
-
-    print(f"Stock Length is: {stock_length}")
-    print(f"Total Stock Length is: {totalStock}")
-
-    print("-" * 80)
-    print("Input cut list")
-
-    cut_list = []
-    answer = "y"
-    while answer == "y":
-        cut_list = add_to_cut_list()
-        print("Current Length is: " + str(sum(cut_list)) + " mm")
-
-        answer = input("do you have more to add? y/n?: ")
-
-    print("-" * 80)
-
-    if sum(cut_list) > totalStock:
-        print("Not enough stock.")
-        quit()
-    else:
-        print("There is potentially enough stock, optimisation possible")
-
-    cut_list.sort()
-    print(cut_list)
-    pack_and_show(cut_list, stock_length)
+    print("total length requested:", sum(cut_lengths))
+    print("total length used:", sum([bin.currently_used for bin in bins]))
